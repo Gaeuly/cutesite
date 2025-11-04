b6fc761629800b0c1b9a7e9a96a61b2e8f81dc01
Gaeuly <7dsgaeul@gmail.com>
2025-11-04 18:01:07 +0700

    edit

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import ProjectCard from "../../components/ProjectCard";
-import { allProjects } from "../../data/projectData";
+import { allProjects } from "../data/projectData";
 
 const ProjectsFull = () => {
   return (
